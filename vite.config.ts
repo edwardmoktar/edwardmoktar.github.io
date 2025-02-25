38f4b1dc0364781119dfa8ea0fe790fe7e9fb64d
edwardmoktar <edwardmoktar2100@gmail.com>
2025-02-26 01:35:47 +0800

    2nd attempt fix github.io site

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,15 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',
+  base: '',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 }));
